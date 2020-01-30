4f91b6bc77f5070f109332e05fa18df17b8b1720
Guuri11 <sergio.gurillo11@gmail.com>
2020-01-30 14:04:01 +0100

    webscrapping sacar precios de una url

--- sacar_precios.py
+++ sacar_precios.py
@@ -1,17 +1,15 @@
 from lxml import html
 import requests
 
-page = requests.get('https://www.amazon.es/s?k=kobe+bryant&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=22WMCVFHZEBZE&sprefix=kobe%2Caps%2C625&ref=nb_sb_ss_i_1_4')
+page = requests.get("http://gurizone.local/catalogo")
 tree = html.fromstring(page.content)
-print(tree)
 
 # Esto creara la lista de compradores
-precio = tree.xpath ('//span[@class="a-price-hole"]/text()')
+producto = tree.xpath ('//h3/a/text()')
 # Esto creara la lista de precios
-producto = tree.xpath('//span[@class="a-text-normal"]/text()')
-
-print('Productos: %s',producto)
-print('Precios: %s',precio)
+precio = tree.xpath('//p[contains(@class,"font-weight-bold")]/text()')
+for i in range(len(producto)):
+    print('El producto '+producto[i]+' cuesta '+precio[i])
 
 # lxml servira para recorrer el html de la pagina solicitada
 # con requests obtenemos el html de una pagina y la almacenamos
